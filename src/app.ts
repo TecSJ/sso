9b310e90695e52fb32d7d448b48040218962e1c2
Zenitramhgx <zenitramhgx15@hotmail.com>
2024-10-04 10:47:12 -0600

    Prueba

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,18 @@ import accesosRoutes from './routes/accesosRoutes';
 import modulosRoutes from './routes/modulosRoutes';
 import rolesRoutes from './routes/rolesRoutes';
 import gruposRoutes from './routes/gruposRoutes';
+import aplicacionesRoutes from './routes/aplicacionesRoutes';
 
 dotenv.config();
 
 
-const app = express(); 
+const app = express();
 app.use(express.json());
 
 app.use('/api', modulosRoutes);
 app.use('/api', rolesRoutes);
 app.use('/api', gruposRoutes);
+app.use('/api', aplicacionesRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
---
+++ src/services/aplicacionService.ts
@@ -0,0 +1,56 @@
+import { ssoDB } from '../database/connection';
+import { queries } from '../database/aplicacionesQueries';
+
+export const getAllAplicaciones = async () => {
+    try {
+        const db = await ssoDB();
+        const [aplicaciones]=await db.execute(queries.getAllAplicaciones);
+        console.log(aplicaciones);
+        return aplicaciones;
+    } catch (error) {
+
+    }
+}
+
+export const getAplicacionById = async (idAplicacion: number) => {
+    try {
+        const db = await ssoDB();
+        const [aplicaciones]=await db.execute(queries.getAplicacionById, [idAplicacion]);
+        console.log(aplicaciones);
+        return aplicaciones;
+    } catch (error) {
+
+    }
+}
+
+export const deleteAplicacionById = async (idAplicacion: number) => {
+    try {
+        const db = await ssoDB();
+        const [aplicacion]=await db.execute(queries.deleteAplicacionById, [idAplicacion]);
+        console.log(aplicacion);
+        return aplicacion;
+    } catch (error) {
+
+    }
+}
+
+export const insertAplicacion = async (clave: string, nombre: string, redireccion: string, estado: string) => {
+    try {
+        const db = await ssoDB();
+        const [result] = await db.execute(queries.insertAplicacion, [clave, nombre, redireccion, estado]);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const updateAplicacionById = async (idAplicacion: number, clave: string, nombre: string, redireccion: string, estado: string) => {
+    try {
+        const db = await ssoDB();
+        const [result] = await db.execute(queries.updateAplicacionById, [idAplicacion, clave, nombre, redireccion, estado]);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
