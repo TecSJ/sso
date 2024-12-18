b58adaefa6ac826470ad7ac4750547cfbd40b690
Zenitramhgx <zenitramhgx15@hotmail.com>
2024-12-18 09:49:04 -0600

    SSO-15: fix: deleteSesion fixed

--- src/routes/sesiones.ts
+++ src/routes/sesiones.ts
@@ -4,7 +4,7 @@ import * as sesiones from '../controllers/sesiones';
 const router = Router();
 
 router.post('/', sesiones.getSesion );
-router.delete('/', sesiones.deleteSesion );
+router.delete('/:idCredencial', sesiones.deleteSesion );
 router.post('/is-valid', sesiones.getValidacion );
 router.post('/is-auth', sesiones.getAuntenticacion );
 router.patch('/:idCredencial/set-contrasena', sesiones.setPassword );
