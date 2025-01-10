049ece03c6c18a3fcc737406ca53504f663ab509
AngelRysu <jesus.quecam@gmail.com>
2025-01-10 16:54:59 -0600

    Se cambia query

--- src/queries/asociacion.ts
+++ src/queries/asociacion.ts
@@ -1,6 +1,11 @@
 export const queries = {
-    getAsociaciones: `SELECT A.*, E.nombre FROM Asociacion AS A INNER JOIN Etiquetas AS E ON (A.idEtiqueta = E.idEtiqueta) `,
-    getAsociacion: `SELECT * FROM Asociacion WHERE idCredencial = ?;`,
+    getAsociaciones: `SELECT A.*, E.nombre 
+    FROM Asociacion AS A 
+    INNER JOIN Etiquetas AS E ON (A.idEtiqueta = E.idEtiqueta) `,
+    getAsociacion: `SELECT A.*, E.nombre 
+    FROM Asociacion AS A
+    INNER JOIN Etiquetas AS E ON (A.idEtiqueta = E.idEtiqueta) 
+    WHERE idCredencial = ?;`,
     deleteAsociacion: `CALL proc_delete_asociacion(?, ?);`,
     insertAsociacion: `CALL proc_insert_asociacion(?, ?);`,
 }
