9e0715ca5dafa62650cb9d8eb2aa4817c52ec2bd
AngelRysu <jesus.quecam@gmail.com>
2025-01-21 15:31:15 -0600

    se actualizan queries para ordenar modulos y aplicacioens

--- src/queries/aplicaciones.ts
+++ src/queries/aplicaciones.ts
@@ -1,5 +1,5 @@
 export const queries = {
-    getAplicaciones: `SELECT * FROM Aplicaciones WHERE estado = 'Activo'`,
+    getAplicaciones: `SELECT * FROM Aplicaciones WHERE estado = 'Activo' ORDER BY nombre;`,
     getAplicacion: `SELECT * FROM Aplicaciones WHERE idAplicacion = ?;`,
     deleteAplicacion: `CALL proc_delete_aplicaciones(?);`,
     insertAplicacion: `CALL proc_insert_aplicaciones(?, ?, ? );`,
--- src/queries/modulos.ts
+++ src/queries/modulos.ts
@@ -1,7 +1,11 @@
 import { QueryBuilder } from "../model/QueryBuilder"
-
+//ordenados por la clave de la aplicacion
 export const queries = {
-    getModulos: `SELECT * FROM Modulos WHERE estado = 'Activo'`,
+    getModulos: `SELECT m.idModulo, m.idAplicacion, m.clave, m.nombre, a.clave AS aplicacion, m.estado
+    FROM Modulos AS m 
+    INNER JOIN Aplicaciones AS a ON m.idAplicacion = a.idAplicacion
+    WHERE m.estado = 'Activo' AND a.estado = 'Activo' 
+    ORDER BY a.clave, m.nombre;`,
     getModulo: `SELECT * FROM Modulos WHERE idModulo = ?;`,
     insertModulo: `CALL proc_insert_modulos(?, ?, ?);`,
     updateModulo: `CALL proc_update_modulos(?, ?, ?, ?);`,
