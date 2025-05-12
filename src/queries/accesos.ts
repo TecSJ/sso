20d8d9824ef9f719ecbc5aa62496e5ec220d1f89
AngelRysu <jesus.quecam@gmail.com>
2025-05-12 12:58:59 -0600

    Actualizacion de queris

--- src/queries/accesos.ts
+++ src/queries/accesos.ts
@@ -1,6 +1,6 @@
 export const queries = {
     getAccesos: `SELECT * FROM sql_accesos_rol WHERE idRol = ? `,
     deleteAcceso: `CALL proc_delete_accesos(?, ?);`,
-    insertAcceso: `CALL proc_insert_accesos(?, ?, ?, ?, ?, ?, ? );`,
-    updateAcceso: `CALL proc_update_accesos(?, ?, ?, ?, ?, ?, ? );`
+    insertAcceso: `CALL proc_insert_accesos(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+    updateAcceso: `CALL proc_update_accesos(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
 }
