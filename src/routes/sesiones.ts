f18156a88321d0262a2452a921c120906a4cd1b0
Jesus Angel Quezada Camacho <jesusangelquezada@MacBook-Pro-de-Jesus.local>
2025-06-23 13:37:08 -0600

    Se agrega endpoint para mandar correo (edcore)

--- src/routes/sesiones.ts
+++ src/routes/sesiones.ts
@@ -10,5 +10,6 @@ router.post('/is-valid', sesiones.getValidacion );
 router.post('/is-auth', sesiones.getAuntenticacion );
 router.patch('/:idCredencial/set-contrasena', sesiones.setPassword );
 router.get('/:idCredencial/data', sesiones.getData);
+router.post('/mail', sesiones.mail);
 
 export default router;
