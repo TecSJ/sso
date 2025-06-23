b6d423f8b3fa6e9e7a0874baca4e4170ca00bef5
Jesus Angel Quezada Camacho <jesusangelquezada@MacBook-Pro-de-Jesus.local>
2025-06-23 13:44:02 -0600

    Se agrega type llaves

---
+++ src/types/Llaves.ts
@@ -0,0 +1,9 @@
+export default interface Llaves {
+    idLlave: number;
+    idCredencial: string;
+    ubicacion: string;
+    fechaCreacion: Date;
+    curp: string;
+    estado: boolean;
+  }
+  
